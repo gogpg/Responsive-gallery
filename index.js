a604fa4cf4a9e55b39b020b2c9b793f5dca88128
Giedrė <giedre.luko@gmail.com>
2022-11-10 21:16:06 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// const hero = document.querySelector('.hero-section')
+
+// let rowLength = 2;
+// function createRow(){
+//     for (let i = 0; i < rowLength; i++) {
+//         const row = document.createElement('div')
+//         hero.appendChild(row)
+//         row.classList.add('row')
+//     }
+// }
+// createRow()
